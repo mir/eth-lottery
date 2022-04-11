df656e4b93d5dba617be53eb4b38105da7338508
Marat Yuldashev <marat.spb@protonmail.com>
2022-04-11 17:06:36 +0300

    Solidity 0.8 + random number from chainlink

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.6;
+pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract Lottery is Ownable {
+contract Lottery is Ownable, VRFConsumerBaseV2 {
 
     address payable[] players;
     uint256 public usdEntranceFee;
@@ -16,17 +18,73 @@ contract Lottery is Ownable {
         WINNER_CALCULATED
     }
     LotteryState public lotteryState;
+    address public winner;
 
-    constructor(address _priceFeedAddress) public {
+    VRFCoordinatorV2Interface COORDINATOR;
+
+     // Your subscription ID.
+    uint64 s_subscriptionId = 2671;
+
+    // Rinkeby coordinator. For other networks,
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+
+    // The gas lane to use, which specifies the maximum gas price to bump to.
+    // For a list of available gas lanes on each network,
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+
+    // Depends on the number of requested values that you want sent to the
+    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
+    // so 100,000 is a safe default for this example contract. Test and adjust
+    // this limit based on the network that you select, the size of the request,
+    // and the processing of the callback request in the fulfillRandomWords()
+    // function.
+    uint32 callbackGasLimit = 100000;
+
+    // The default is 3, but you can set this higher.
+    uint16 requestConfirmations = 3;
+
+    // For this example, retrieve 2 random values in one request.
+    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+    uint32 numWords =  1;
+    
+    uint256 public s_requestId;
+    address s_owner;
+
+    constructor(address _priceFeedAddress) VRFConsumerBaseV2(vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_owner = msg.sender;
+        
         usdEntranceFee = 50 * 10**18; //18 decimals precision
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lotteryState = LotteryState.CLOSED;
     }
 
+    // Assumes the subscription is funded sufficiently.
+    function requestRandomWords() external onlyOwner {
+        // Will revert if subscription is not set and funded.
+        s_requestId = COORDINATOR.requestRandomWords(
+        keyHash,
+        s_subscriptionId,
+        requestConfirmations,
+        callbackGasLimit,
+        numWords
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 randomWord = randomWords[0];
+        endLottery(randomWord);
+    }
+
     function enter() public payable {
         require(lotteryState == LotteryState.OPEN);
         require(msg.value >= getEntranceFee(), "Not enough ETH to enter the lottery");        
-        players.push(msg.sender);
+        players.push(payable(msg.sender));
     }
 
     function getEntranceFee() public view returns(uint256) {
@@ -48,11 +106,11 @@ contract Lottery is Ownable {
 
     function startLottery() public onlyOwner {
         require(lotteryState == LotteryState.CLOSED, "Lottery should be closed");
-        lotteryState = LotteryState.OPEN
+        lotteryState = LotteryState.OPEN;
     }
 
-    function endLottery() public onlyOwner {
+    function endLottery(uint256 randomNumber) public onlyOwner {
         require(lotteryState == LotteryState.OPEN, "Lottery should be open");
-        lotteryState = LotteryState.WINNER_CALCULATED
+        lotteryState = LotteryState.WINNER_CALCULATED;
     }
 }
